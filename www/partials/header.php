7128b3550caa7328a283acd479191896e6a71148
SymphonyOfFear <124155888+SymphonyOfFear@users.noreply.github.com>
2024-03-20 19:07:12 +0100

    Fixed header

--- www/partials/header.php
+++ www/partials/header.php
@@ -2,8 +2,8 @@
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
-
-
+$loggedIn = isset($_SESSION['isLoggedIn']) ? $_SESSION['isLoggedIn'] : false;
+$userRole = isset($_SESSION['userRole']) ? $_SESSION['userRole'] : 'guest'; // Default to 'guest' if not set.
 ?>
 
 <body>
@@ -16,12 +16,12 @@ if (session_status() == PHP_SESSION_NONE) {
                 <li><a href="../index.php">Home</a></li>
                 <li><a href="../views/menu.php">Menu</a></li>
                 <li><a href="../views/reserveren.php">Reserveren</a></li>
-                <?php if (isset($_SESSION['isLoggedIn']) && $_SESSION($userRole['userRole'] == "manager") || $_SESSION($userRole['userRole'] == 'director')) : ?>
-                    <a href="../views/admin_dashboard.php">Admin Dashboard</a>
-                <?php elseif (isset($_SESSION['isLoggedIn']) && $_SESSION($userRole['userRole'] == "employee")) : ?>
-                    <a href="../views/employee-dashboard.php">Medewerker Dashboard</a>
-                <?php elseif (isset($_SESSION['isLoggedIn']) && $_SESSION($userRole['userRole'] == "customer")) : ?>
-                    <a href="../views/dashboard.php">Dashboard</a>
+                <?php if ($loggedIn && ($userRole == "manager" || $userRole == 'director')) : ?>
+                    <li><a href="../views/admin_dashboard.php">Admin Dashboard</a></li>
+                <?php elseif ($loggedIn && $userRole == "employee") : ?>
+                    <li><a href="../views/employee-dashboard.php">Medewerker Dashboard</a></li>
+                <?php elseif ($loggedIn && $userRole == "customer") : ?>
+                    <li><a href="../views/dashboard.php">Dashboard</a></li>
                 <?php endif; ?>
 
                 <li class="dropdown">
@@ -29,15 +29,14 @@ if (session_status() == PHP_SESSION_NONE) {
                     <div class="dropdown-content">
                         <?php if ($loggedIn) : ?>
                             <a href="../views/instellingen.php">Instellingen</a>
-                            <a href="../controllers/uitloggen.php">Uitloggen</a>
-
+                            <a href="../controllers/
+                            uitloggen.php">Uitloggen</a>
                         <?php else : ?>
                             <a href="../views/inloggen.php">Inloggen</a>
                             <a href="../views/registreren.php">Registreren</a>
                         <?php endif; ?>
                     </div>
                 </li>
-
             </ul>
         </nav>
     </header>
